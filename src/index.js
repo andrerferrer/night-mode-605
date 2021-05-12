3ba5bc7175017cf2604a0badf053b82f5a39dc29
andre ferrer <andrerferrer@hotmail.com>
2021-05-12 04:09:29 -0300

    Night mode complete

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-// Todo
+// select the element
+const btn = document.querySelector('button');
+// add event listener
+btn.addEventListener('click', (event) => {
+  // implement the callback
+  document.querySelector('body')
+          .classList
+          .toggle('bg-dark');
+})
